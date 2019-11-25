7203a26a982f459b11b8a2d8634f5f19b2ab7e96
dmknght <dmknght@localhost.localdomain>
2019-11-25 20:59:29 +0700

    Add new template clamav gui

---
+++ src/main.nim
@@ -0,0 +1,51 @@
+import gintro / [gtk, gobject]
+
+proc main =
+  gtk.init()
+  let mainBoard = newWindow()
+  mainBoard.title = "Parrot ClamAV"
+
+  let
+    boxMain = newBox(Orientation.vertical, 5)
+    boxScan = newBox(Orientation.horizontal, 5)
+    btnQuickScan = newButton("Quick Scan")
+    btnFullScan = newButton("Full Scan")
+    btnCustomScan = newButton("Custom Scan")
+    # TODO online analysis + repu scan
+
+  boxScan.packStart(btnQuickScan, false, true, 3)
+  boxScan.packStart(btnFullScan, false, true, 3)
+  boxScan.packStart(btnCustomScan, false, true, 3)
+  boxMain.packStart(boxScan, false, true, 3)
+
+  let
+    boxProtection = newBox(Orientation.horizontal, 5)
+    btnUpdate = newButton("Update")
+    # TODO real time protection
+    # TODO update settings?
+
+  boxProtection.packStart(btnUpdate, false, true, 3)
+  boxMain.add(boxProtection)
+
+  let
+    boxHistory = newBox(Orientation.horizontal, 5)
+    btnLog = newButton("Log")
+    btnQuaratine = newButton("Quaratine")
+
+  boxHistory.packStart(btnLog, false, true, 3)
+  boxHistory.packSTart(btnQuaratine, false, true, 3)
+  boxMain.add(boxHistory)
+
+  let
+    boxOptions = newBox(Orientation.horizontal, 5)
+    btnSettings = newButton("Settings")
+  
+  boxOptions.packStart(btnSettings, false, true, 3)
+
+  boxMain.packStart(boxOptions, false, true, 3)
+  mainBoard.add(boxMain)
+
+  mainBoard.showAll
+  gtk.main()
+
+main()
