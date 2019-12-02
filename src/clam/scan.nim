e49e6f2e9816f3c9553a9f8c5b553cd1c9f76ee1
dmknght <dmknght@localhost.localdomain>
2019-12-02 20:56:54 +0700

    New layout for scanner

--- src/clam/scan.nim
+++ src/clam/scan.nim
@@ -5,9 +5,30 @@ import gintro / gtk
 proc scan(path: string, b: Button, asRoot = false) =
   let
     scanDialog = newDialog()
-    actionStop = scanDialog.addButton("Stop", 0)
+    scanLabel = newLabel("Scanning") # TODO status here: preparing / scanning / completed
+    boxButtons = newBox(Orientation.horizontal, 3)
+    # scanned = newLabel("File scanned: ")
+    # Threats = newLabel("Threats: ")
+    scanProgress = newProgressBar()
+    scanArea = scanDialog.getContentArea()
+    btnStop = newButton("Stop")
+    btnMinimize = newButton("Hide")
+    imgStop = newImageFromIconName("exit", 3) # if not compelted else dialog-yes
+    imgMinimize = newImageFromIconName("gtk-go-down", 3)
+    # TODO minimize button
 
-  scanDialog.title = "Scanning"
+  scanLabel.setXalign(0.0)
+
+  btnStop.setImage(imgStop)
+  btnMinimize.setImage(imgMinimize)
+
+  boxButtons.packStart(btnMinimize, false, true, 3)
+  boxButtons.packStart(btnStop, false, true, 3)
+
+  scanArea.packStart(scanLabel, false, true, 3)
+  scanArea.packStart(scanProgress, false, true, 9)
+  scanArea.packStart(boxButtons, false, true, 0)
+  scanDialog.title = "Scanning " & path # TODO Custom scan or something else; add completed to title
   scanDialog.setDefaultSize(400, 100)
   scanDialog.showAll
 
