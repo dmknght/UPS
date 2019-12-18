ed33f5ea161b05b4ed317135a711b944389e4c34
dmknght <dmknght@localhost.localdomain>
2019-12-19 01:48:44 +0700

    Try new code with the spawnAsync

--- src/clamgui/scan.nim
+++ src/clamgui/scan.nim
@@ -1,4 +1,4 @@
-import gintro / [gtk, gobject]
+import gintro / [gtk, gobject, glib]
 
 import strutils # Test only
 import ../ clamcontrol / controller
@@ -6,9 +6,39 @@ import osproc
 import streams
 
 
-proc scan(path: string, b: Button, asRoot = false) =
+# proc scan(scanLabel: Label) =
+#   let
+#     path = "/home/dmknght/" # TODO edit here. Must be tuple or some shit
+#     p = startProcess(command = "/usr/bin/clamscan", args = ["--no-summary", "-v", path])
+#     msg = outputStream(p)
+  
+#   var line = ""
+#   # let
+#   #   # p = startProcess(command = "/usr/bin/clamscan", args = ["--no-summary", "-v", path])
+#   #   msg = outputStream(p)
+#   # var line = ""
+#   # while msg.readLine(line):
+#   #   scanLabel.setLabel(line)
+
+#    # while p.running():
+#   while msg.readLine(line):
+#     scanLabel.setLabel(line)
+#   #   scanDialog.queueDraw()
+#   #   while eventsPending():
+#   #     if not mainIteration():
+#   #       echo line
+#   #       scanLabel.setLabel(line)
+#   #       scanDialog.queueDraw()
+#   p.close()
+
+proc initScan(d: Dialog): bool =
+  queueDraw(d)
+  return SOURCE_CONTINUE
+
+proc scanController(path: string, b: Button, asRoot = false) =
   let
     scanDialog = newDialog()
+    scanLabel = newLabel("Preparing") # TODO status here: preparing / scanning / completed
     boxButtons = newBox(Orientation.horizontal, 3)
     # scanned = newLabel("File scanned: ")
     # Threats = newLabel("Threats: ")
@@ -20,18 +50,19 @@ proc scan(path: string, b: Button, asRoot = false) =
     imgStop = newImageFromIconName("exit", 3) # if not compelted else dialog-yes
     imgMinimize = newImageFromIconName("go-down", 3)
     # TODO minimize button
-    p = startProcess(command = "/usr/bin/clamscan", args = ["--no-summary", "-v", path])
+    # p = startProcess(command = "/usr/bin/clamscan", args = ["--no-summary", "-v", path])
     # PROCESS RUN UNTIL IT STOP AND THE WINDOWS SHOW
     # pid = p.processID()
-  var
-    scanLabel = newLabel("Preparing") # TODO status here: preparing / scanning / completed
-
-  scanDialog.queueDraw()
+    # command = ["/usr/bin/clamscan", "--no-summary", "-v", path]
+  
   scanLabel.setXalign(0.0)
-
+  var procID: int
+  discard spawnAsync("", ["/usr/bin/clamscan", "--no-summary", "-v", path], [], {doNotReapChild}, nil, nil, procID)
+ 
   # Set image for buttons
   btnStop.setImage(imgStop)
-  btnStop.connect("clicked", controller.actionStop, p)
+  # btnStop.connect("clicked", controller.actionStop, p)
+  # TODO bug when process is completed. Close is above
   # TODO cancel or do something close windoww
   btnHide.setImage(imgMinimize)
   btnHide.connect("clicked", controller.actionHide, scanDialog)
@@ -46,28 +77,34 @@ proc scan(path: string, b: Button, asRoot = false) =
   scanDialog.title = "Scanning " & path # TODO Custom scan or something else; add completed to title
   scanDialog.setDefaultSize(400, 100)
   
+  discard timeoutAdd(1, initScan, scanDialog)
+
   # let
-  #   # p = startProcess(command = "/usr/bin/clamscan", args = ["--no-summary", "-v", path])
+  #   p = startProcess(command = "/usr/bin/clamscan", args = ["--no-summary", "-v", path])
   #   msg = outputStream(p)
   # var line = ""
   # while msg.readLine(line):
   #   scanLabel.setLabel(line)
-  p.close()
-
-  # btnStop.connect("clicked", controller.actionStop, p)
+  # p.close()
+  
   scanDialog.showAll
+  # btnStop.connect("clicked", controller.actionStop, p)
+  
+  # discard timeoutAdd(1, scan, scanLabel)
+  # connect(scanDialog, "clicked", scan, scanLabel)
+  # scanDialog.showAll
   
   # let testValue = p.outputStream()
   # var line: string
   # while testValue.readLine(line):
   #   scanLabel.label = line
 
-
 proc quickScan*(b: Button) =
   let path = "/home/dmknght/Templates/"
+  b.setLabel("Scanning Home") # TODO restore label
   # TODO handle stop
-  scan(path, b)
+  scanController(path, b)
 
 proc fullScan*(b: Button) =
   let path = "/"
-  scan(path, b, asRoot = true)
+  scanController(path, b, asRoot = true)
