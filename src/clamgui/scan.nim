a19492bf1a76b347255fa77e4ba7a5bc06f41989
dmknght <dmknght@localhost.localdomain>
2020-01-04 08:18:56 +0700

    try new object and add check running

--- src/clamgui/scan.nim
+++ src/clamgui/scan.nim
@@ -5,14 +5,24 @@ import strutils
 
 import utils
 
+type
+  Scanner = object
+    stdChannel: Channel[string]
+    scanPath: string # TODO edit here for custom scan
+    scanTitle: string
+    mainThread: system.Thread[tuple[objScan: ref Scanner]]
+    options: seq[string]
+    # TODO is root
+
 var
-  globalChan: Channel[string]
-  watcherThread: system.Thread[tuple[path: string]]
+  objHomeScan: ref Scanner = new(Scanner)
+  objFullScan: ref Scanner = new(Scanner)
+  globalChannel: Channel[string]
 
-# TODO handle multiple threads
 
-proc watchProc(interval: tuple[path: string]) {.thread.} =
-  let scanner = startProcess("/usr/bin/clamscan", args = @["--no-summary", "-v", interval.path])
+proc procScanner(argv: tuple[objScan: ref Scanner]) {.gcsafe.} =
+  # TODO craft options and pass args
+  let scanner = startProcess("/usr/bin/clamscan", args = @["--no-summary", "-v", "-r", argv.objScan.scanPath])
   var
     numScan = 0
     numClean = 0
@@ -23,7 +33,8 @@ proc watchProc(interval: tuple[path: string]) {.thread.} =
       let scanResult = scanner.outputStream.readLine()
       if scanResult.split(" ")[0] == "Scanning":
         numScan += 1
-        globalChan.send(scanResult)
+        # argv.objScan.stdChannel.send("Scanning " & scanResult.split("/")[^1])
+        globalChannel.send("Scanning " & scanResult.split("/")[^1])
       elif scanResult.split(" ")[^1] == "OK":
         numClean += 1 
       elif scanResult.split(" ")[^1] == "FOUND":
@@ -31,17 +42,21 @@ proc watchProc(interval: tuple[path: string]) {.thread.} =
         # TODO malware type = scanResult.split(" ")[^2]
     except system.IOError:
       let msg = "Scan completed!\nScanned: " & intToStr(numScan) & " Clean: " & intToStr(numClean) & " Found: " & intToStr(numInfect)
-      globalChan.send(msg)
+      # argv.objScan.stdChannel.send(msg)
+      globalChannel.send(msg)
 
 
-proc recvCb(scanLabel: Label): bool = 
-  let data = globalChan.tryRecv()
+# proc scanPipe(argv: tuple[scanLabel: Label, objScanner: ref Scanner]): bool = 
+proc scanPipe(scanLabel: Label): bool = 
+  # let data = argv.objScanner.stdChannel.tryRecv()
+  let data = globalChannel.tryRecv()
   if data[0]:
+    # argv.scanLabel.setText(data[1])
     scanLabel.setText(data[1])
   return SOURCE_CONTINUE
 
 
-proc createScan(path: string, title: string, b: Button, asRoot = false) =
+proc createScan(b: Button, scanObj: ref Scanner) =
   let
     scanDialog = newDialog()
     scanLabel = newLabel("Preparing")
@@ -56,6 +71,8 @@ proc createScan(path: string, title: string, b: Button, asRoot = false) =
 
   
   scanLabel.setXalign(0.0)
+  scanLabel.setLineWrap(true)
+  scanLabel.setMaxWidthChars(30)
   
   # Set image for buttons
   btnStop.setImage(imgStop)
@@ -72,30 +89,44 @@ proc createScan(path: string, title: string, b: Button, asRoot = false) =
   areaScan.packStart(scanProgress, false, true, 9)
   areaScan.packStart(boxButtons, false, true, 0)
   
-  scanDialog.title = title
+  scanDialog.title = scanObj.scanTitle
   scanDialog.setDefaultSize(400, 100)
 
-  discard timeoutAdd(70, recvCb, scanLabel)
+  discard timeoutAdd(5, scanPipe, scanLabel)
   # TODO handle multiple channels
-  globalChan.open()
-  createThread(watcherThread, watchProc, (path,))
+  # scanObj.stdChannel.open()
+  globalChannel.open()
+  if not objHomeScan.mainThread.running():
+    createThread(scanObj.mainThread, procScanner, (scanObj,))
+  else:
+    discard # echo "It is running in background"
   scanDialog.showAll
 
 
 proc homeScan*(b: Button) =
   # TODO get environment path
-  let
-    path = glib.getHomeDir()
-    title = "Scanning " & path
-  # TODO handle stop
-  createScan(path, title, b)
+  # let
+  #   path = glib.getHomeDir() & "/Templates" # TODO edit here
+  #   title = "Scanning " & path
+
+  #   threadHomeScan = Scanner(
+  #     isRunning: false,
+  #     stdChannel: Channel[string],
+  #     path: glib.getHomeDir() & "/Templates",
+  #     title: "Scanning " & path,
+  #     mainThread: system.Thread[tuple[path: string]],
+  #     options: seq[string]
+  #   )
+  # # TODO handle stop
+  objHomeScan.scanPath = glib.getHomeDir() & "/Templates"
+  objHomeScan.scanTitle = "Scanning " & objHomeScan.scanPath
+  createScan(b, objHomeScan)
 
 
 proc fullScan*(b: Button) =
-  let
-    path = "/"
-    title = "Scanning System"
-  createScan(path, title, b, asRoot = true)
+  objFullScan.scanPath = glib.getHomeDir() & "/Documents"
+  objFullScan.scanTitle = "Scanning " & objFullScan.scanPath
+  createScan(b, objFullScan) # TODO edit obj here
 
 
 proc customScan*(b: Button) =
@@ -103,4 +134,4 @@ proc customScan*(b: Button) =
     path = "/home"
     # TODO edit path to list of files / folders
     title = "Custom Scan"
-  createScan(path, title, b, asRoot = false)
+  createScan(b, objHomeScan) # TODO edit obj here
